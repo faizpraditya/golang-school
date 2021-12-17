c00dc1930b78fe31240fd504c05e4936eeeb411b
pradityamf <pradityamf@gmail.com>
2021-12-17 16:58:42 +0700

    [FEATURE] Create main menu

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 func main() {
-
 	// NewDbConn()
 	db := NewDbConn()
 	defer db.Close()
 
 	// Migrate table
 	db.Migration(Student{}, Subject{})
+
+	MainMenu(db)
 }
---
+++ menu.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var scanner = bufio.NewScanner(os.Stdin)
+
+func MainMenu(db *DBConn) {
+	fmt.Println("Enigma School")
+	fmt.Println("1. Data Mahasiswa")
+	fmt.Println("2. Subject/Mata Kuliah")
+	fmt.Println("3. Mahasiswa pilih Mata Kuliah")
+	fmt.Println("4. Tampilkan data Mahasiswa dengan list Mata Kuliah yang diambil berdasarkan nim")
+	fmt.Println("5. Exit")
+	fmt.Print("Pilih Menu:")
+	scanner.Scan()
+	menu, _ := strconv.Atoi(scanner.Text())
+	mainMenuController(menu, db)
+}
+
+func mainMenuController(menu int, db *DBConn) {
+	switch menu {
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		fmt.Println("Exit from program")
+		os.Exit(1)
+	default:
+		defer MainMenu(db)
+		fmt.Println("Wrong input")
+		return
+	}
+}
